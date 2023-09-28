5be9fdd2f4ebdfc5d7f76c6eac61115babd42ef4
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-09-28 20:32:03 +0500

    Task4

---
+++ Tyuiu.BogdanovaSA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BogdanovaSA.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.BogdanovaSA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BogdanovaSA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.BogdanovaSA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        Assert.AreEqual(10, DataService.Subtraction(10, 5)
+        
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Multiplication(10, 5));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }   
+}
