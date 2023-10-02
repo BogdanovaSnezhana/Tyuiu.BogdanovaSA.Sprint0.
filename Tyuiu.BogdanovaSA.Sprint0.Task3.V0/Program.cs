ff09e9a2d8ca28266cf2aac8edc405104e716b7e
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-10-02 16:30:55 +0500

    3

--- Tyuiu.BogdanovaSA.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.BogdanovaSA.Sprint0.Task3.V0/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.BogdanovaSA.Sprint0.Task3.V0.Lib
+using Tyuiu.BogdanovaSA.Sprint0.Task3.V0.Lib;
 
 namespace Tyuiu.BogdanovaSA.Sprint0.Task3.V0
 {
